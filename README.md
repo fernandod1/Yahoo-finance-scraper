c950e9072aa649a56636ff8bbe388be48c405624
dlfernando <antiope@gmail.com>
2020-02-02 19:39:46 +0100

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Yahoo-finance-scraper
-This python script scrap "Open" and "Previous Close" values from any company in Yahoo Finance and save them in a local text file.
+This python script scrap "Open" and "Previous Close" values from Yahoo Finance Apple Inc.
+ https://finance.yahoo.com/quote/AAPL?p=AAPL or any other company and save them in a local text file.
+
+REQUIREMENTS:
+
+Python V3, modules requests and re.
+
+CONFIGURATION:
+
+Open "yahoofinance.py", modify line number 38 to indicate desired full path of output file generated:
+
+filename = "financevalues.txt"
+
+EXECUTE:
+
+To execute script under Windows, open console window and write:
+
+python yahoofinance.py
+
---
+++ yahoofinance.py
@@ -0,0 +1,50 @@
+import re
+import sys
+import requests
+
+# Copyright (c) 2020 Fernando
+# Url: https://github.com/dlfernando/
+# License: MIT
+
+def writefile(filename, value):
+    try:
+        f= open(filename, "w+",  encoding="utf-8")
+        f.write(value)
+        f.close()
+        print(f"Values saved in {filename} file.")
+    except:
+        print(f"Could not write to {filename} file.")
+
+def scrapdata():
+    headers = {
+        "Referer": 'https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch',
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+    }
+    r = requests.get(f"https://finance.yahoo.com/quote/AAPL?p=AAPL", headers=headers)    
+    openvalue = re.search(r'\"AAPL\":{\"sourceInterval\":(.*?),\"regularMarketOpen\":{\"raw\":(.*?),\"', r.text)
+    if openvalue is not None:
+        openvalue = openvalue.group(2)
+        print(f"OPEN: {openvalue}")
+    else:
+        openvalue = ""
+        print("NO OPEN value found")
+    previousvalue = re.search(r'\"summaryDetail\":{\"previousClose\":{\"raw\":(.*?),\"', r.text)
+    if previousvalue is not None:
+        previousvalue = previousvalue.group(1)
+        print(f"PREVIOUS: {previousvalue}") 
+    else:
+        print("NO PREVIOUS value found")
+        previousvalue = ""
+    data = 'OPEN: '+str(openvalue)+'\nPREVIOUS: '+str(previousvalue)
+    return data
+
+
+def main():
+    filename = "financevalues.txt"
+    value = scrapdata()
+    writefile(filename, value)
+
+if __name__ == "__main__":
+
+    main()
+
